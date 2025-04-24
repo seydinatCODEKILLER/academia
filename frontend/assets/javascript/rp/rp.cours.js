f6c201e0aeded103aa37527dd651889a32d84619
Ben Thiam <seydinat235@gmail.com>
2025-04-24 23:36:23 +0000

    Mise en place de l'interface cours du responsable pedagogique

---
+++ frontend/assets/javascript/rp/rp.cours.js
@@ -0,0 +1,10 @@
+import { initRouter } from "../../../router/router.js";
+import { getCurrentUser } from "../../../store/authStore.js";
+import { handleRpSidebar, renderRpHeader } from "../../../utils/rp.utils.js";
+
+document.addEventListener("DOMContentLoaded", async () => {
+  initRouter();
+  const user = getCurrentUser();
+  handleRpSidebar(user);
+  renderRpHeader(user, "Mes cours");
+});
