0afd02529fe2375b5b890a78e91af5cad70583a5
Ben Thiam <seydinat235@gmail.com>
2025-04-30 23:51:57 +0000

    Mise en place de l'interface du cours pour l'etudiant

---
+++ frontend/assets/javascript/etudiant/etudiant.cours.js
@@ -0,0 +1,17 @@
+import { initRouter } from "../../../router/router.js";
+import { getCurrentUser } from "../../../store/authStore.js";
+import { handleNotifications } from "../../../store/notificationStore.js";
+import { getIdEtudiantByUserId } from "../../../services/etudiantService.js";
+import {
+  handleEtudiantSidebar,
+  renderEtudiantHeader,
+} from "../../../utils/etudiant.utils.js";
+
+document.addEventListener("DOMContentLoaded", async () => {
+  initRouter();
+  handleNotifications();
+  const user = getCurrentUser();
+  handleEtudiantSidebar(user);
+  renderEtudiantHeader(user, "Cours");
+  const idEtudiant = await getIdEtudiantByUserId(user.id);
+});
